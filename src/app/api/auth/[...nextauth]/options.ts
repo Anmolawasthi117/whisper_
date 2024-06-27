b272e13505ae5d1b86361deb01fd98a65f4189f9
Anmol <anmolawasthi117@gmail.com>
2024-06-27 23:23:07 +0530

    secret updated

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -66,7 +66,7 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: "sexa",
   pages: {
     signIn: '/sign-in',
   },
